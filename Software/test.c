2b5655abb986f57dd5ccd3f1216eb1d8f3f98787
GreySup <slonebrownstone@gmail.com>
2021-03-04 14:36:01 +0100

    added type macro

--- Software/test.c
+++ Software/test.c
@@ -2,17 +2,20 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/time.h>
+#include <linux/input.h>
 
+#define TYPE(t) t##
+/*
 typedef struct input_event {
     struct timeval time;
     unsigned short type;
     unsigned short code;
     unsigned int value;
 } input_event;
-
+*/
 int main(int argc, char** argv) {
     FILE *mouse;
-    input_event event;
+    struct input_event event;
 
     mouse = fopen("/dev/input/event0", "r");
     if(mouse == NULL) {
@@ -24,11 +27,11 @@ int main(int argc, char** argv) {
     while(1) {
         size_t bytes = fread(&event, sizeof(struct input_event), 1, mouse);
 	
-	if (bytes > 0) {
-	    printf("************ EVENT %d ************\nsec: %ld, usec: %ld type: %hu, code: %hu, value: %d\n",
-                i, event.time.tv_sec, event.time.tv_usec, event.type, event.code, event.value);
-	    i++;
-	}
+        if (bytes > 0) {
+            printf("************ EVENT %d ************\nsec: %ld, usec: %ld type: %hu, TYPE: %s,code: %hu, value: %d\n",
+                    i, event.time.tv_sec, event.time.tv_usec, event.type, TYPE(event.type), event.code, event.value);
+            i++;
+        }
     }
     return EXIT_SUCCESS;
 }
