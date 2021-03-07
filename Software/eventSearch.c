dd48506584c662252eda1fed56d1b759a4fd6675
GreySup <slonebrownstone@gmail.com>
2021-03-07 01:45:37 +0100

    fixed eventSearch.c

--- Software/eventSearch.c
+++ Software/eventSearch.c
@@ -2,49 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
-#include <fcntl.h>
-#include <sys/time.h>
-#include <linux/input.h>
 
-char* getEvent(){
-
+bool getEvent(FILE *event, char *token){
 	char *out = NULL;
-    	size_t line_buf_size = 0;
-        FILE *event;
-	char *token;
-	
-	
-	event = fopen("/proc/bus/input/devices","r");
-	
-	
-	 if(event == NULL) {
-        	printf("ERROR Opening %s\n", event);
-        	return EXIT_FAILURE;
-	}
-
+    size_t line_buf_size = 0;
 
 	while(getline(&out, &line_buf_size, event)>0){
-
 		if(strstr(out, "Handlers=mouse0")){
-		
-		token = strtok(out, " ");
-		token=strtok(NULL, " ");
-		token=strtok(NULL, " ");
-		return token;
-  		 
+			token = strtok(out, " ");
+			token = strtok(NULL, " ");
+			token = strtok(NULL, " ");
+			return true;
 		}
 	}
-
-	return EXIT_FAILURE;
-
+	return false;
 }
 
 
-int main(int argc, char *argv[])
-{
-	printf("%s\n",getEvent());
-
-return 0;
-
+int main(int argc, char *argv[]){
+	char *token;
+	FILE *event;
+	event = fopen("/proc/bus/input/devices","r");
 	
+	if(event == NULL) {
+		printf("ERROR Opening %s\n", event);
+		return EXIT_FAILURE;
+	}
+
+	if (getEvent(event, token))
+		printf("%s\n", token);
+	else
+		printf("Not found\n");
+	return 0;
 }
