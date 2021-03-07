cb6f4a3130e92c68172d7f5bd001936a02233865
RootsMatt <53291542+RootsMatt@users.noreply.github.com>
2021-03-07 01:00:58 +0100

    Add files via upload

---
+++ Software/eventSearch.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <linux/input.h>
+
+char* getEvent(){
+
+	char *out = NULL;
+    	size_t line_buf_size = 0;
+        FILE *event;
+	char *token;
+	
+	
+	event = fopen("/proc/bus/input/devices","r");
+	
+	
+	 if(event == NULL) {
+        	printf("ERROR Opening %s\n", event);
+        	return EXIT_FAILURE;
+	}
+
+
+	while(getline(&out, &line_buf_size, event)>0){
+
+		if(strstr(out, "Handlers=mouse0")){
+		
+		token = strtok(out, " ");
+		token=strtok(NULL, " ");
+		token=strtok(NULL, " ");
+		return token;
+  		 
+		}
+	}
+
+	return EXIT_FAILURE;
+
+}
+
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n",getEvent());
+
+return 0;
+
+	
+}
